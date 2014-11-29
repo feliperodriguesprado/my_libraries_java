9b4eb393f9080c481e40a5551ff06ac610c22279
Diego Nunes <diegodnunes12@gmail.com>
2014-11-29 16:53:38 -0200

    Melhorias e criação da empréstimos.XHTML

--- src/main/java/tk/mylibraries/controller/EmprestimosController.java
+++ src/main/java/tk/mylibraries/controller/EmprestimosController.java
@@ -4,19 +4,83 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.annotation.ManagedBean;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
 
+import tk.mylibraries.dao.BibliotecaDAO;
 import tk.mylibraries.dao.EmprestimoDAO;
+import tk.mylibraries.dao.TipoBibliotecaDAO;
 import tk.mylibraries.entities.Biblioteca;
 import tk.mylibraries.entities.Emprestimo;
+import tk.mylibraries.entities.TipoBiblioteca;
 import tk.mylibraries.orm.HibernateUtil;
 
-@ManagedBean(value = "emprestimoBean")
+/**
+ * @author Diego
+ *
+ */
+@ManagedBean
+@ViewScoped
 public class EmprestimosController {
 
 	Emprestimo emprestimo = new Emprestimo();
+	Biblioteca biblioteca = new Biblioteca();
+	TipoBiblioteca tipo = new TipoBiblioteca();
 	private EmprestimoDAO dao;
-	List emprestimos = new ArrayList();
+	private List emprestimos;
+	private List<Biblioteca> library;
+
+	public List<Biblioteca> getLibrary() {
+		return library;
+	}
+
+	public void setLibrary(List<Biblioteca> library) {
+		this.library = library;
+	}
+
+	public TipoBiblioteca getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoBiblioteca tipo) {
+		this.tipo = tipo;
+	}
+
+	// insere as bibliotecas cadastras no combobox de bibliotecas
+	public List<SelectItem> getTipos() {
+		System.out.println("Chegou nessa bosta");
+		List<TipoBiblioteca> tipos = new TipoBibliotecaDAO(
+				HibernateUtil.getEntityManager()).getAll();
+		List<SelectItem> lib = new ArrayList<SelectItem>(tipos.size());
+		for (TipoBiblioteca p : tipos) {
+			System.out.println(p.getNome());
+			lib.add(new SelectItem(p.getTipoId(), p.getNome()));
+		}
+		return lib;
+	}
+
+	public void getBibliotecas() {
+		if (tipo != null && !tipo.equals("")) {
+			List<Biblioteca> library = new EmprestimoDAO(
+					HibernateUtil.getEntityManager())
+					.getBibliotecasPeloTipo(tipo.getTipoId());
+			
+		}
+		// System.out.println("Chegou nessa bosta");
+		// List<Biblioteca> bibliotecas = new BibliotecaDAO(
+		// HibernateUtil.getEntityManager()).getAll();
+		// List<SelectItem> bib = new ArrayList<SelectItem>(bibliotecas.size());
+		// for (Biblioteca p : bibliotecas) {
+		// System.out.println(p.getNome());
+		// bib.add(new SelectItem(p.getBibliotecaId(), p.getNome()));
+		// }
+		// return bib;
+	}
+
+	public Biblioteca getBiblioteca() {
+		return biblioteca;
+	}
 
 	public EmprestimosController() {
 		emprestimos = new EmprestimoDAO(HibernateUtil.getEntityManager())
@@ -42,9 +106,11 @@ public class EmprestimosController {
 		emprestimo.setBiblioteca(biblioteca);
 	}
 
-	public Biblioteca getBiblioteca() {
-		return emprestimo.getBiblioteca();
-	}
+	// public Biblioteca getBiblioteca() {
+	// bibliotecas = new
+	// BibliotecaDAO(HibernateUtil.getEntityManager()).getAll();
+	// return emprestimo.getBiblioteca();
+	// }
 
 	public void setDataEmprestimo(Date data_emprestimo) {
 		emprestimo.setDataEmprestimo(data_emprestimo);
--- src/main/java/tk/mylibraries/dao/EmprestimoDAO.java
+++ src/main/java/tk/mylibraries/dao/EmprestimoDAO.java
@@ -1,12 +1,31 @@
 package tk.mylibraries.dao;
 
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
+import tk.mylibraries.entities.Biblioteca;
 import tk.mylibraries.entities.Emprestimo;
 
-public class EmprestimoDAO extends GenericDAO < Emprestimo , Long >{
+public class EmprestimoDAO extends GenericDAO<Emprestimo, Long> {
 	public EmprestimoDAO(EntityManager em) {
 		super(em);
 	}
- }
+
+	public List<Biblioteca> getBibliotecasPeloTipo(long id) {
+
+		try {
+			String query = "from Biblioteca b where b.tipoid = :id";
+			TypedQuery<Biblioteca> typedQuery = em.createQuery(query,
+					Biblioteca.class);
+			typedQuery.setParameter("tipoid", id);
+			List<Biblioteca> bibliotecas = typedQuery.getResultList();
+			return bibliotecas;
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+}
