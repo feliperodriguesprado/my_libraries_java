0d7a2061da9ec6ac5932b892f7cd92e7c4d1f5dc
João Paulo Nakajima <joaopaulonakajima@gmail.com>
2014-11-30 15:43:20 -0200

    Biblioteca Controller
    termino da biblioteca controller

--- src/main/java/tk/mylibraries/controller/BibliotecaController.java
+++ src/main/java/tk/mylibraries/controller/BibliotecaController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
 import javax.servlet.http.HttpSession;
 
 import tk.mylibraries.dao.BibliotecaDAO;
@@ -20,18 +22,22 @@ import tk.mylibraries.utils.WebUtils;
 
 @ManagedBean
 public class BibliotecaController {
-	
+
 	private Biblioteca biblioteca;
 	private BibliotecaDAO bibliotecaDAO;
 	private List<TipoBiblioteca> tipoBibliotecaList;
 	private List<ClassificacaoBiblioteca> classificacaoBibliotecaList;
 	private TipoBiblioteca tipoBiblioteca;
 	private ClassificacaoBiblioteca classificacaoBiblioteca;
-	
+	private List<Biblioteca> list1;
+	private List<Biblioteca> list2;
+	private List<Biblioteca> list3;
+
 	public BibliotecaController() {
 		bibliotecaDAO = new BibliotecaDAO(HibernateUtil.getEntityManager());
 		biblioteca = new Biblioteca();
-		classificacaoBibliotecaList = new ClassificacaoBibliotecaController().getAll();
+		classificacaoBibliotecaList = new ClassificacaoBibliotecaController()
+				.getAll();
 		tipoBibliotecaList = new TipoBibliotecaController().getAll();
 		tipoBiblioteca = new TipoBiblioteca();
 		classificacaoBiblioteca = new ClassificacaoBiblioteca();
@@ -46,7 +52,8 @@ public class BibliotecaController {
 		biblioteca.setTipoBiblioteca(tipoBiblioteca);
 
 		FacesContext context = FacesContext.getCurrentInstance();
-		HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+		HttpSession session = (HttpSession) context.getExternalContext()
+				.getSession(false);
 		Long userId = (Long) session.getAttribute("user");
 		EntityManager entityManager = HibernateUtil.getEntityManager();
 		UsuarioDAO usuarioDAO = new UsuarioDAO(entityManager);
@@ -56,18 +63,38 @@ public class BibliotecaController {
 
 		bibliotecaDAO.save(biblioteca);
 	}
-	
-	public List<Object> getTipoBibliotecaByUser() {
+
+	public void getTipoBibliotecaByUser() {
+
+		List<Object> listAll = new ArrayList<Object>();
 		long id = WebUtils.getInstance().getIdUserSession();
 		UsuarioDAO usuarioDAO = new UsuarioDAO(HibernateUtil.getEntityManager());
 		Usuario usuario = usuarioDAO.getById(id);
-		
-		TipoBibliotecaDAO tipoBibliotecaDAO = new TipoBibliotecaDAO(HibernateUtil.getEntityManager());
+
+		TipoBibliotecaDAO tipoBibliotecaDAO = new TipoBibliotecaDAO(
+				HibernateUtil.getEntityManager());
 		List<TipoBiblioteca> listTipos = tipoBibliotecaDAO.getAll();
-		List<TipoBiblioteca> listTiposDoUsuario = new ArrayList<TipoBiblioteca>();
+		EntityManager em = HibernateUtil.getEntityManager();
+		WebUtils webUtils = WebUtils.getInstance();
+		long idUsuario = webUtils.getIdUserSession();
+
 		for (TipoBiblioteca tipoBiblioteca : listTipos) {
-			select 
+			long idTipo = tipoBiblioteca.getTipoId();
+
+			try {
+				String query = "select * from biblioteca join tipo_biblioteca on tipo_biblioteca.tipoid = biblioteca.bibliotecaid join usuario on usuario.usuarioid = biblioeca.usuarioid where usuarioid = :idUsuario and tipo_biblioteca.tipoid = :idTipo";
+				Query q = em.createQuery(query);
+				q.setParameter("idUsuario", idUsuario);
+				q.setParameter("idTipo", idTipo);
+				Object result = q.getResultList();
+				listAll.add(result);
+			} catch (NoResultException e) {
+			}
 		}
+		list1 = (List<Biblioteca>) listAll.get(0);
+		list2 = (List<Biblioteca>) listAll.get(1);
+		list3 = (List<Biblioteca>) listAll.get(2);
+
 	}
 
 	public List<Biblioteca> getAll() {
@@ -122,4 +149,17 @@ public class BibliotecaController {
 	public List<ClassificacaoBiblioteca> getClassificacaoBibliotecaList() {
 		return classificacaoBibliotecaList;
 	}
+
+	public List<Biblioteca> getList1() {
+		return list1;
+	}
+
+	public List<Biblioteca> getList2() {
+		return list2;
+	}
+
+	public List<Biblioteca> getList3() {
+		return list3;
+	}
+
 }
