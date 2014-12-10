2c3901ef3d8c31be193b759f960183ab7bf7a436
Diego Nunes <diegodnunes12@gmail.com>
2014-12-10 20:47:02 -0200

    Melhorias no emprestimo

--- src/main/java/tk/mylibraries/controller/EmprestimoController.java
+++ src/main/java/tk/mylibraries/controller/EmprestimoController.java
@@ -1,11 +1,15 @@
 package tk.mylibraries.controller;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import tk.mylibraries.dao.BibliotecaDAO;
 import tk.mylibraries.dao.EmprestimoDAO;
@@ -18,33 +22,108 @@ import tk.mylibraries.utils.Messages;
 import tk.mylibraries.utils.WebUtils;
 
 @ManagedBean
-public class EmprestimoController {
-	
+@ViewScoped
+public class EmprestimoController implements Serializable {
+
 	private Emprestimo emprestimo;
 	private TipoBiblioteca tipoBiblioteca;
 	private Biblioteca biblioteca;
 	private BibliotecaDAO bibliotecaDAO;
-
+	private String destinatario;
+	private Date dataEmprestimo;
+	private String observacao;
 	private TipoBibliotecaDAO tipoBibliotecaDAO;
 	private EmprestimoDAO emprestimoDAO;
-	
 	private List<TipoBiblioteca> typeLibraryList;
 	private Map<String, Long> typeLibraryMap;
-	
 	private List<Biblioteca> libraryList;
 	private Map<String, Long> libraryMap;
-	
 	private String idTypeLibrary;
 	private String idLibrary;
 	
+	private List<Emprestimo> listaEmprestimoL;
+	private List<Emprestimo> listaEmprestimoM;
+	private List<Emprestimo> listaEmprestimoV;
+
+	public List<Emprestimo> getListaEmprestimoL() {
+		return listaEmprestimoL;
+	}
+
+	public void setListaEmprestimo(List<Emprestimo> listaEmprestimoL) {
+		this.listaEmprestimoL = listaEmprestimoL;
+	}
+
+	public List<Emprestimo> getListaEmprestimoM() {
+		return listaEmprestimoM;
+	}
+
+	public void setListaEmprestimoM(List<Emprestimo> listaEmprestimoM) {
+		this.listaEmprestimoM = listaEmprestimoM;
+	}
+
+	public List<Emprestimo> getListaEmprestimoV() {
+		return listaEmprestimoV;
+	}
+
+	public void setListaEmprestimoV(List<Emprestimo> listaEmprestimoV) {
+		this.listaEmprestimoV = listaEmprestimoV;
+	}
+
+	public String getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(String destinatario) {
+		this.destinatario = destinatario;
+	}
+
+	public Date getDataEmprestimo() {
+		return dataEmprestimo;
+	}
+
+	public void setDataEmprestimo(Date dataEmprestimo) {
+		this.dataEmprestimo = dataEmprestimo;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
 	public EmprestimoController() {
-		tipoBibliotecaDAO = new TipoBibliotecaDAO(HibernateUtil.getEntityManager());
+		emprestimo = new Emprestimo();
+		tipoBibliotecaDAO = new TipoBibliotecaDAO(
+				HibernateUtil.getEntityManager());
 		emprestimoDAO = new EmprestimoDAO(HibernateUtil.getEntityManager());
 		tipoBiblioteca = new TipoBiblioteca();
 		bibliotecaDAO = new BibliotecaDAO(HibernateUtil.getEntityManager());
 		biblioteca = new Biblioteca();
+		listaEmprestimoL = new ArrayList<Emprestimo>();
+		listaEmprestimoM = new ArrayList<Emprestimo>();
+		listaEmprestimoV = new ArrayList<Emprestimo>();
+		listasDeEmprestimos();
 	}
 	
+	public void listasDeEmprestimos(){
+		List<Emprestimo> emp = new ArrayList<Emprestimo>();
+		emp = emprestimoDAO.getAll();
+		for(Emprestimo emprestimo: emp){
+			if(emprestimo.getBiblioteca().getBibliotecaId() == 1)
+				listaEmprestimoL.add(emprestimo);
+			else{
+				if(emprestimo.getBiblioteca().getBibliotecaId() == 2)
+					listaEmprestimoV.add(emprestimo);
+				else{
+					listaEmprestimoM.add(emprestimo);
+				}
+			}			
+			//System.out.println(emprestimo.getDestinatario());
+		}
+	}
+
 	/**
 	 * Metodo que popula o combo box do tipo de biblioteca
 	 */
@@ -53,63 +132,71 @@ public class EmprestimoController {
 		typeLibraryList = tipoBibliotecaDAO.getAll();
 		typeLibraryMap = new HashMap<String, Long>();
 		for (TipoBiblioteca tipoBiblioteca : typeLibraryList) {
-			typeLibraryMap.put(tipoBiblioteca.getNome(), tipoBiblioteca.getTipoId());
+			typeLibraryMap.put(tipoBiblioteca.getNome(),
+					tipoBiblioteca.getTipoId());
 		}
 	}
-	
+
 	/**
 	 * Metodo que popula o combo box das bibliotecas de acordo com o usuário
 	 * logado.
 	 * 
-	 * @param tipoBiblioteca {@link TipoBiblioteca}
+	 * @param tipoBiblioteca
+	 *            {@link TipoBiblioteca}
 	 */
 	public void setLibraryMap(TipoBiblioteca tipoBiblioteca) {
-		
+
 		libraryList = emprestimoDAO.getBibliotecasPeloTipo(tipoBiblioteca);
-		
+
 		libraryMap = new HashMap<String, Long>();
-		
+
 		if (libraryList != null) {
-			
+
 			for (Biblioteca biblioteca : libraryList) {
-				
-				if (biblioteca.getUsuario().getUsuarioId() == WebUtils.getInstance().getIdUserSession()) {
-					libraryMap.put(biblioteca.getNome(), biblioteca.getBibliotecaId());						
+
+				if (biblioteca.getUsuario().getUsuarioId() == WebUtils
+						.getInstance().getIdUserSession()) {
+					libraryMap.put(biblioteca.getNome(),
+							biblioteca.getBibliotecaId());
 				}
-			
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	/**
 	 * Metodo que sera chamado quando for selecionado um item do combo box tipo
 	 * de biblioteca
 	 */
 	public void onTypeLibraryChange() {
-		
+
 		if (idTypeLibrary.length() > 0 && idTypeLibrary != null) {
-			
+
 			for (TipoBiblioteca tipoBiblioteca : typeLibraryList) {
 				if (tipoBiblioteca.getTipoId() == Long.parseLong(idTypeLibrary)) {
 					setLibraryMap(tipoBiblioteca);
 					break;
 				}
 			}
-			
+
 		} else {
 			libraryMap = new HashMap<String, Long>();
 		}
-		
+
 	}
-	
+
 	public void save() {
+		Biblioteca idLib = bibliotecaDAO.getById(Long.parseLong(idLibrary));
+		emprestimo.setBiblioteca(idLib);
+		emprestimo.setDataEmprestimo(getDataEmprestimo());
+		emprestimo.setDestinatario(getDestinatario());
+		emprestimo.setObservacao(getObservacao());
+		emprestimo.setAtivo(true);
 		emprestimoDAO.save(emprestimo);
-		System.out.println(emprestimo.getBiblioteca());
-		Messages.getInstance().setMessageInfo("Dados:", "ID tipo = " + idTypeLibrary + ", ID biblioteca = " + idLibrary);
 	}
-	
+
 	public Map<String, Long> getTypeLibraryMap() {
 		return typeLibraryMap;
 	}
@@ -117,7 +204,7 @@ public class EmprestimoController {
 	public Map<String, Long> getLibraryMap() {
 		return libraryMap;
 	}
-	
+
 	public String getIdTypeLibrary() {
 		return idTypeLibrary;
 	}
@@ -125,15 +212,13 @@ public class EmprestimoController {
 	public void setIdTypeLibrary(String idTypeLibrary) {
 		this.idTypeLibrary = idTypeLibrary;
 	}
-	
+
 	public String getIdLibrary() {
 		return idLibrary;
 	}
 
 	public void setIdLibrary(String idLibrary) {
-		biblioteca = bibliotecaDAO.getById(Long.parseLong(idLibrary));
-		emprestimo.setBiblioteca(biblioteca);
+		this.idLibrary = idLibrary;
 	}
-	
 
 }
--- src/main/java/tk/mylibraries/entities/Emprestimo.java
+++ src/main/java/tk/mylibraries/entities/Emprestimo.java
@@ -24,20 +24,20 @@ public class Emprestimo implements Serializable {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "emprestimo_emprestimoid_seq")
 	@SequenceGenerator(name = "emprestimo_emprestimoid_seq", sequenceName = "emprestimo_emprestimoid_seq", allocationSize = 1)
 	private Long emprestimoId;
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "bibliotecaid")
 	private Biblioteca biblioteca;
-	@Column(name = "data_emprestimo", nullable = false)
+	@Column(name = "data_emprestimo", nullable = true)
 	@Temporal(TemporalType.DATE)
 	private Date dataEmprestimo;
-	@Column(name = "data_encerramento", nullable = false)
+	@Column(name = "data_encerramento", nullable = true)
 	@Temporal(TemporalType.DATE)
 	private Date dataEncerramento;
-	@Column(name = "destinatario", nullable = false, length = 100)
+	@Column(name = "destinatario", nullable = true, length = 100)
 	private String destinatario;
 	@Column(name = "observacao", length = 200)
 	private String observacao;
-	@Column(name = "ativo", nullable = false)
+	@Column(name = "ativo", nullable = true)
 	private Boolean ativo;
 
 	public Long getEmprestimoId() {
