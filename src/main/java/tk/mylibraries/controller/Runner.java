a07bb8b63fd83e16a1f37eb0adfe5b818a46e9e5
Felipe Rodrigues <rodriguesprado.felipe@gmail.com>
2014-12-10 23:51:42 -0200

    Runner.

--- src/main/java/tk/mylibraries/controller/Runner.java
+++ src/main/java/tk/mylibraries/controller/Runner.java
@@ -3,34 +3,6 @@ package tk.mylibraries.controller;
 public class Runner {
 
 	public static void main(String[] args) {
-
-
-		EntityManager entityManager = HibernateUtil.getEntityManager();
-		System.out.println(entityManager.toString());	
-		
-		ClassificacaoBibliotecaController bibliotecaController = new ClassificacaoBibliotecaController();
-		TipoBibliotecaController bibliotecaController2 = new TipoBibliotecaController();
-		
-		BibliotecaController bb = new BibliotecaController();
-		List<ClassificacaoBiblioteca> list = new ArrayList<ClassificacaoBiblioteca>();
-		ClassificacaoBibliotecaDAO classificacaoBibliotecaDAO = new ClassificacaoBibliotecaDAO(HibernateUtil.getEntityManager());
-		list = classificacaoBibliotecaDAO.getAll();
-		
-		ClassificacaoBiblioteca classificacaoBiblioteca = new ClassificacaoBiblioteca();
-		classificacaoBiblioteca = list.get(1);
-		
-		List<TipoBiblioteca> list2 = new ArrayList<TipoBiblioteca>();
-		TipoBiblioteca tipoBiblioteca = new TipoBiblioteca();
-		TipoBibliotecaDAO tipoBibliotecaDAO = new TipoBibliotecaDAO(HibernateUtil.getEntityManager());
-		list2 = tipoBibliotecaDAO.getAll();
-		tipoBiblioteca = list2.get(1);
-		
-//		bb.setClassificacaoBiblioteca(classificacaoBiblioteca);
-//		bb.setTipoBiblioteca(tipoBiblioteca);
-		bb.setNome("Corrida Mortal");
-		bb.setDesejado(true);
-		
-		bb.salvar();
 		
 	}
 }
